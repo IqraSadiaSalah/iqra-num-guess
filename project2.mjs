73ee9ac5577746e248739f0de7c495ffd8cf11c3
iqra <iqrasadia505@gmail.com>
2022-12-17 14:53:42 +0500

    commit

---
+++ project2.mjs
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+const randomNumber = Math.round(Math.random() * 10);
+let status = false;
+//console.log(randomNumber);
+while (!status) {
+    const userInput = await inquirer.prompt({
+        name: 'user_guess',
+        type: "number",
+        message: "Guess the number...",
+    });
+    if (userInput.user_guess === randomNumber) {
+        status = true;
+        console.log(chalk.magentaBright("Hurray! Guess was right..."));
+    }
+    else {
+        console.log("Opppsiii!! Try Again....");
+    }
+}
